e66ed63b59bef96b02ed61b55672c8f7040ffdc5
Eliott Houget <eliott.houget@ens.rennes.fr>
2025-11-14 13:47:05 +0100

    progress on monkey.ml

--- bin/monkey.ml
+++ bin/monkey.ml
@@ -1,131 +1,110 @@
-open Notty_unix
-open Ui
 open Utils
 open Effect
 open Effect.Deep
 open Engine
 open Entity
 open World
-open File
+open Heap
 
-(** [get_cell_camel ()]
-    renvoie la position du chameau*)
-let get_pos_camel () : int*int = 
-  let rec boucle (i : int) (j : int) : int*int = 
-    if i >= width then failwith "pas de chameau" 
+(** [get_cell_camel ()] renvoie la position du chameau*)
+let get_pos_camel () : int * int =
+  let rec boucle (i : int) (j : int) : int * int =
+    if i >= width then failwith "pas de chameau"
     else begin
-      if j >= height then boucle (i+1) 0 
-      else match get (i,j) with
-        | Camel -> (i,j)
-        | _ -> boucle i (j+1)
+      if j >= height then boucle (i + 1) 0
+      else match get (i, j) with
+      | Camel -> (i, j)
+      | _ -> boucle i (j + 1)
     end
   in
   boucle 0 0
 
-let abs (x : int) = if x>=0 then x else (-x)
+let abs (x : int) = if x >= 0 then x else -x
 
-(** [distance]
-  calcul la distance de Manhattan entre deux positions*)
-let distance (pos1 : int*int) (pos2 : int*int) : int = 
-  let (l1,c1) = pos1 in
-  let (l2,c2) = pos2 in
-  (abs (l1 - l2) + abs (c1 - c2))
+(** [distance] calcul la distance de Manhattan entre deux positions*)
+let distance (pos1 : int * int) (pos2 : int * int) : int =
+  let l1, c1 = pos1 in
+  let l2, c2 = pos2 in
+  abs (l1 - l2) + abs (c1 - c2)
 
-
-let reconstruct_path (map : (int*int) array array) (goal : int*int) (start : int*int) : (int*int) list =
-  let rec aux p acc = 
-    if p = start then acc 
-    else let (l,c) = p in aux (map.(l).(c)) (p::acc)
+(** [reconstruct_path] reconstruit le chemin de [start] à [goal] pour le singe
+*)
+let reconstruct_path (map : (int * int) array array) (goal : int * int)
+    (start : int * int) : (int * int) list =
+  let rec reconstruct_from_goal position path_acc =
+    if position = start then path_acc
+    else
+      let l, c = position in
+      reconstruct_from_goal map.(l).(c) (position :: path_acc)
   in
-  aux goal []
+  reconstruct_from_goal goal []
 
-let get_neighbors (position : int*int) : (int*int) list =
-  let (l,c) = position in
+(** [get_neighbors] renvoie la liste des voisins [Empty] ou [Camel] de [pos] *)
+let get_neighbors (position : int * int) : (int * int) list =
+  let l, c = position in
   let is_empty_or_camel pos =
-    match get pos with
-      |Empty |Camel -> true
-      |_ -> false
+    match get pos with Empty | Camel -> true | _ -> false
   in
-  List.filter is_empty_or_camel [(l+1,c);(l-1,c);(l,c+1);(l,c-1)]
+  List.filter is_empty_or_camel
+    [ (l + 1, c); (l - 1, c); (l, c + 1); (l, c - 1) ]
 
-let pop_min (liste : ((int*int)*int) list) : ((int*int)*(((int*int)*int) list)) = 
-  let rec cherche l acc x_min v_min = match l with
-    |[] -> (x_min,acc)
-    |(x,v)::q -> if v<v_min then cherche q ((x_min,v_min)::acc) x v
-      else cherche q ((x,v)::acc) x_min v_min
-  in
-  match liste with 
-  |[] -> failwith "impossible de pop dans une liste vide" 
-  |(x1,v1)::lst -> cherche lst [] x1 v1
-
-let appartient (liste : ((int*int)*int) list) (x : (int*int)) : bool =
-  let rec cherche l = match l with
-    |[] -> false
-    |(y,_)::q -> if x=y then true else cherche q
-  in
-  cherche liste
+(** [compare elt1 elt2] compare deux sommets pondérés selon leur poids *)
+let compare (elt1 : (int * int) * int) (elt2 : (int * int) * int) : bool =
+  let _, prio1 = elt1 in
+  let _, prio2 = elt2 in
+  prio1 >= prio2
 
 exception No_path_found
 
-let a_star (start : int*int) (goal : int*int) (h : (int*int) -> (int*int) -> int) : (int*int) list =
-  let map = Array.make_matrix width height (-1,-1) in
-  let vrai_scores = Array.make_matrix width height max_int in 
-  let faux_scores = Array.make_matrix width height max_int in
-  let (ls,cs) = start in
-  map.(ls).(cs) <- (ls,cs);
-  vrai_scores.(ls).(cs) <- 0;
-  faux_scores.(ls).(cs) <- h start goal;
-  let rec upgrade lst_voisins lc cc file = 
-    match lst_voisins with
-    |[] -> file
-    |(l,c)::q -> let tentative_vrai_score = vrai_scores.(lc).(cc) + 1 in
-      if tentative_vrai_score < vrai_scores.(l).(c) then (
-        map.(l).(c) <- (lc,cc);
-        vrai_scores.(l).(c) <- tentative_vrai_score;
-        faux_scores.(l).(c) <- tentative_vrai_score + (h (l,c) goal);
-        if appartient file (l,c) then 
-          upgrade q lc cc file 
-        else 
-          upgrade q lc cc (((l,c),faux_scores.(l).(c))::file)
+(** [a_star start goal h] renvoit un chemin de [start] à [goal] selon l'heuristique [h]*)
+let a_star (start : int * int) (goal : int * int) (h : int * int -> int * int -> int) : (int * int) list =
+  let map = Array.make_matrix width height (-1, -1) in (* Pour reconstruire le chemin *)
+  let distance = Array.make_matrix width height max_int in 
+  let goal_found = ref false in
+
+  let x, y = start in
+  map.(x).(y) <- start;
+  distance.(x).(y) <- 0;
+  let file = Heap.init (width*height) compare (start, 0) in
+
+  while (not !goal_found) && (not (is_empty file)) do
+    let (x, y), _ = Heap.pop file in
+    Printf.printf "%d %d\n" x y;
+    if (x, y) = goal then
+      goal_found := true
+    else (
+      let voisins = get_neighbors (x, y) in
+      let upgrade (voisin : (int * int)) =
+        let x', y' = voisin in
+        let nouvelle_distance = distance.(x).(y) + 1 in
+        if nouvelle_distance < distance.(x').(y') then (
+          map.(x').(y') <- (x, y);
+          distance.(x').(y') <- nouvelle_distance;
+          let faux_scores = nouvelle_distance + h voisin goal in
+          Heap.push file ((x', y'), faux_scores);
         )
-      else upgrade q lc cc file
-  in
-  let rec boucle file = 
-    if file = [] then 
-      raise No_path_found
-    else 
-      begin
-        let current,new_file = pop_min file in
-        if current = goal then 
-          reconstruct_path map goal start 
-        else 
-          begin
-            let (lc,cc) = current in
-            let lst_voisins = get_neighbors (lc,cc) in
-            boucle (upgrade lst_voisins lc cc new_file)
-          end
-      end
-  in
-  boucle [((ls,cs),0)]
+      in
+      List.iter upgrade voisins
+    )
+  done;
 
-    
+  if !goal_found then
+    reconstruct_path map start goal
+  else
+    raise No_path_found
 
+(** [next_pos_a_star] cherche le prochain mouvement à faire pour le singe depuis
+    [pos] selon [a_star] *)
+let next_pos_a_star (pos : int * int) : int * int =
+  match a_star pos (get_pos_camel ()) distance with
+  | [] -> failwith "pas possible"
+  | p :: _ -> p
 
-
-
-
-
-
-let next (pos : int*int) : int*int= 
-  match a_star pos (get_pos_camel()) distance with
-    |[] -> failwith "pas possible"
-    |p::_ -> p
-
-(** [snake current_pos] effectue tous les prochains tours du serpent à partir de la position 
-    [current_pos] (choisir aléatoirement une entrée, se déplacer en conséquence, recommencer)*)
+(** [monkey] cherche le prochain mouvement pour le singe selon [a_star] et
+    l'effectue depuis sa position *)
 let rec monkey (monkey_instance : entity) : unit =
   let current_position = monkey_instance#get_pos in
-  let new_position = next current_position in
+  let new_position = next_pos_a_star current_position in
   let new_position = move current_position new_position in
   monkey_instance#set_pos new_position;
   perform End_of_turn;
